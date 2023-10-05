f289d55c0578b958fbf48b267beece2af165cfcb
yasermoamd <yasermoamd@hotmail.com>
2023-10-05 13:24:38 +0100

    init: project

---
+++ app/core/App.php
@@ -0,0 +1,13 @@
+<?php
+class App {
+
+    public function __construct() 
+    {
+        echo "Ok!";
+    }
+
+    public function parseUrl() 
+    {
+        //TODO
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+include '../app/config/connection.php';
+require_once '../vendor/autoload.php';
+require_once '../app/init.php';
+
+
+
+$app = new App;
